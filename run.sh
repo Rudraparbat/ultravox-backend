57a1f6593a087aeb127a3e0a5648a2bf1592da4d
Ruddy <rudraparbat714@gmail.com>
2025-12-22 18:35:17 +0530

    removed lmcache

--- run.sh
+++ run.sh
@@ -44,25 +44,20 @@ export VLLM_WORKER_MULTIPROC_METHOD=spawn
 export TORCH_COMPILE_DISABLE=1
 export PYTHONHASHSEED=0
 
-export LMCACHE_CHUNK_SIZE=32
-export LMCACHE_LOCAL_CPU=True
-export LMCACHE_MAX_LOCAL_CPU_SIZE=12
-
 
 exec vllm serve fixie-ai/ultravox-v0_5-llama-3_2-1b \
-    --kv-offloading-backend lmcache \
-    --kv-offloading-size 12 \
-    --disable-hybrid-kv-cache-manager \
     --host 0.0.0.0 \
     --port 8000 \
     --trust-remote-code \
     --dtype float16 \
     --max-model-len 1024 \
     --limit-mm-per-prompt '{"audio": 1}' \
-    --gpu-memory-utilization 0.85 \
-    --max-num-seqs 256 \
+    --gpu-memory-utilization 0.95 \
+    --max-num-seqs 16 \
     --attention-backend flash_attn \
     --enable-prefix-caching \
     --disable-log-stats \
-    --long-prefill-token-threshold 32 \
+    --distributed-executor-backend mp \
+    --long-prefill-token-threshold 512 \
     --no-disable-chunked-mm-input \
+    --stream-interval 1
