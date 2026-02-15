9679643bbce4a9bd33e31599189cde8fc4476f0f
Ruddy <rudraparbat714@gmail.com>
2026-02-15 14:42:31 +0530

    fix : change model to llama boned

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ echo "Logging into Hugging Face..."
 huggingface-cli login --token $HF_TOKEN 
 
 echo "Starting vLLM..."
-exec vllm serve fixie-ai/ultravox-v0_6-gemma-3-27b \
+exec vllm serve fixie-ai/ultravox-v0_6-llama-3_1-8b \
   --host 0.0.0.0 \
   --port 8080 \
   --limit-mm-per-prompt '{"audio":20}' \
